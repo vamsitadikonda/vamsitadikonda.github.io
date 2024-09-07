5ddd4fd564ef1b641bbc9770d5561caaef8ef6b3
vamsitadikonda <vamsitadikonda99@gmail.com>
2024-09-07 03:05:39 -0700

    updating color schema to brick red

--- src/styles/variables.js
+++ src/styles/variables.js
@@ -2,17 +2,17 @@ import { css } from 'styled-components';
 
 const variables = css`
   :root {
-    --dark-navy: #020c1b;
-    --navy: #0a192f;
-    --light-navy: #112240;
-    --lightest-navy: #233554;
-    --navy-shadow: rgba(2, 12, 27, 0.7);
-    --dark-slate: #495670;
-    --slate: #8892b0;
-    --light-slate: #a8b2d1;
-    --lightest-slate: #ccd6f6;
+    --dark-navy: #dbd7c9;  
+    --navy: #e3e0d5;        
+    --light-navy: #ebe9e1;
+    --lightest-navy: #fbfaf8;
+    --navy-shadow: rgba(212, 165, 154, 1);
+    --dark-slate: #dab1a8;
+    --slate: #c88c7e;
+    --light-slate: #ae5d4a;
+    --lightest-slate: #924e3e;
     --white: #e6f1ff;
-    --green: #64ffda;
+    --green: #bc4639;
     --green-tint: rgba(100, 255, 218, 0.1);
     --pink: #f57dff;
     --blue: #57cbff;
